c6182c2bc38c933c1a15efb76b2330cf6f5c7871
rabee361 <rha60540@gmail.com>
2025-09-24 20:31:06 +0300

    fix

--- store/settings.py
+++ store/settings.py
@@ -159,11 +159,11 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
